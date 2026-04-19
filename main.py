48aaa61e7ff4213e28cee3cde19eec36b876e7da
NefzgerMatthias <matthias.nefzger@rdf.nuernberg.de>
2026-04-19 13:19:01 +0200

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from backend.smartview_opc.app import app
-
-if __name__ == "__main__":
-    # Dies startet deinen Flask-Server
-    print("Starte SmartView OPC Server auf http://localhost:5000...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
